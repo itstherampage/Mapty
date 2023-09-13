182f4f97cb851bf2bd006c1bd67a02cf6a6c44ae
Logan Wright <loganlwright@gmail.com>
2023-09-13 07:59:43 -0400

    Create Workout class

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  date = new Date();
+
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+
 class App {
   #map;
   #mapEvent;
